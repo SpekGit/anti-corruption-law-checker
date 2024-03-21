a4ca4afcc83fd712c8813dc11286785199cda66f
Haller125 <connect@spektal.com>
2024-03-21 20:13:26 +0500

    created modal to correct the conclusion

---
+++ src/components/modals/ModalSentToCorrectorConclusion.tsx
@@ -0,0 +1,44 @@
+import { t } from 'i18next';
+import React from 'react'
+import { useDispatch } from 'react-redux';
+import { Portal } from '.'
+import IconsContent from '../../assets/icons/IconsContent';
+import { ConclusionsServices } from '../../services/ConclusionsServices';
+import { ProjectsServices } from '../../services/ProjectServices';
+import ModalError from './ModalError';
+import ModalSuccess from './ModalSuccess';
+
+type Props = {
+    close: Function;
+    open: boolean;
+    conclusionId: number
+};
+const ModalSentToCorrectorConclusion: React.FC<Props> = ({ close, open, conclusionId }) => {
+    const dispatch = useDispatch()
+    const [openError, setOpenError] = React.useState<boolean>(false)
+    const [errorMessage, setErrorMessage] = React.useState<any>()
+    const [openSuccess, setOpenSuccess] = React.useState<boolean>(false)
+    const sendToCorrectorConclusion = () => {
+        dispatch({ type: "SET_LOADING_TRUE", payload: true })
+        ConclusionsServices.changeConclusion(conclusionId, { status_id: 11 })
+            .then(() => { dispatch({ type: "SET_LOADING_FALSE" }); setOpenSuccess(true); close(false) })
+            .catch((err) => { dispatch({ type: "SET_LOADING_FALSE" }); setOpenError(true); setErrorMessage(err); close(false) })
+    }
+    return (
+        <>
+            <Portal close={close} open={open}>
+                <p className='modal__icon'>{IconsContent.Ask()}</p>
+                <p className='modal__ask'>{t("send_to_corrector_conclusion")}</p>
+                <div className='modal__buttons'>
+                    <button className='c-btn c-btn-green' onClick={() => sendToCorrectorConclusion()}>{t("confirm")}</button>
+                    <button className='c-btn c-btn-blue' onClick={() => close(false)}>{t("close_modal")}</button>
+                </div>
+            </Portal>
+            <ModalError open={openError} close={setOpenError} errorMessage={errorMessage} />
+            <ModalSuccess open={openSuccess} close={setOpenSuccess} />
+
+        </>
+    )
+}
+
+export default ModalSentToCorrectorConclusion
