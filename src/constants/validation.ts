70a7dee9cab334259d8f04aaadf9cd5ff00bcf84
Haller125 <connect@spektal.com>
2024-03-29 15:52:11 +0500

    Update validation.ts

--- src/constants/validation.ts
+++ src/constants/validation.ts
@@ -103,3 +103,73 @@ export const AddProject = yup.object().shape({
   uploads_ai: yup.mixed().required("error_input_file").test("error_input!", "error_input_file", value => value[0]),
   email: yup.string().email("error_input_email").required("error_input"),
 });
+export const CatalogItem = yup.object().shape({
+  name_kk: yup.string().nullable().required("error_input"),
+  name_ru: yup.string().nullable().required("error_input"),
+  group_kk: yup.string().nullable(),
+  group_ru: yup.string().nullable(),
+  base_cost: yup.string().nullable(),
+  remark_cost: yup.string().nullable(),
+});
+export const AssignByEDS = yup.object().shape({
+  password: yup.string().required("error_input")
+    .matches(/^([A-Za-z0-9.-\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/gi, 'Пароль должен содержать только латинские буквы'),
+  file3: yup.mixed().required("error_input_file").test("error_input!", "error_input_file", value => value[0]),
+});
+
+export const AddUser = yup.object().shape({
+  role: yup.string().required("error_input"),
+  birth_date: yup.string().when('role', { is: (role: string) => role === 'expert' || role === 'coordinator', then: yup.string().required('error_input') }),
+  full_name: yup.string().required("error_input"),
+  email: yup.string().email("error_input_email").required("error_input"),
+  tin: yup.string().required("error_input").test({
+    test: (value) => {
+      if (!value) {
+        return false;
+      } else if (value.replace(/_/g, "").length !== 12) {
+        return false;
+      }
+      return true;
+    },
+    message: "ИИН должен содержать 12 символов",
+  }),
+  password: yup.string().required("error_input")
+    .matches(/^([A-Za-z0-9.-\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/gi, 'Пароль должен содержать только латинские буквы'),
+  scientific_title_kk: yup.string().when('role', { is: 'expert', then: yup.string().required('error_input'), }),
+  scientific_title_ru: yup.string().when('role', { is: 'expert', then: yup.string().required('error_input') }),
+  organization_kk: yup.string().when('role', { is: (role: string) => role === 'expert' || role === 'coordinator', then: yup.string().required('error_input') }),
+  organization_ru: yup.string().when('role', { is: (role: string) => role === 'expert' || role === 'coordinator', then: yup.string().required('error_input') }),
+  academic_degree_kk: yup.string().when('role', { is: 'expert', then: yup.string().required('error_input') }),
+  academic_degree_ru: yup.string().when('role', { is: 'expert', then: yup.string().required('error_input') }),
+  branch_of_legislations: yup.array().when('role', {
+    is: 'expert', then: yup.array().test({
+      test: (value) => {
+        if (value?.length == 0) {
+          return false;
+        } else {
+          return true;
+        }
+      },
+      message: "error_input",
+    }).required('error_input')
+  }),
+  job_title_ru: yup.string().when('role', { is: (role: string) => role === 'expert' || role === 'coordinator', then: yup.string().required('error_input') }),
+  job_title_kk: yup.string().when('role', { is: (role: string) => role === 'expert' || role === 'coordinator', then: yup.string().required('error_input') }),
+  phone: yup.string().when('role', {
+    is: (role: string) => role === 'expert' || role === 'coordinator', then: yup.string().test({
+      test: (value) => {
+        if (!value) {
+          return false;
+        } else if (value.replace(/_/g, "").length !== 18) {
+          return false;
+        }
+        return true;
+      },
+      message: "Некорректный формат номера",
+    }).required('error_input')
+  }),
+})
+
+
+
+
