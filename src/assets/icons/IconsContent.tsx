33198f8e7a8bda28a1f052084cd9d7d7067c0474
Haller125 <connect@spektal.com>
2024-04-01 16:33:11 +0500

    Create IconsContent.tsx

---
+++ src/assets/icons/IconsContent.tsx
@@ -0,0 +1,141 @@
+function AddProject() { // Wrapper profile
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 20H20C20.5304 20 21.0391 19.7893 21.4142 19.4142C21.7893 19.0391 22 18.5304 22 18V8C22 7.46957 21.7893 6.96086 21.4142 6.58579C21.0391 6.21071 20.5304 6 20 6H12.07C11.7406 5.9983 11.4167 5.91525 11.1271 5.75824C10.8375 5.60123 10.5912 5.37512 10.41 5.1L9.59 3.9C9.40882 3.62488 9.1625 3.39877 8.8729 3.24176C8.58331 3.08475 8.25941 3.0017 7.93 3H4C3.46957 3 2.96086 3.21071 2.58579 3.58579C2.21071 3.96086 2 4.46957 2 5V18C2 19.1 2.9 20 4 20Z" stroke="#4ABD9B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M12 10V16" stroke="#4ABD9B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M9 13H15" stroke="#4ABD9B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+);
+}
+function ArrowDown() { // Wrapper profile
+    return (
+        <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1 1.5L6 6.5L11 1.5" stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+);
+}
+function Ask() { //modal confirm ?
+    return (
+        <svg width="80" height="80" viewBox="0 0 58 58" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M29 53.1667C42.3469 53.1667 53.1667 42.3469 53.1667 29C53.1667 15.6532 42.3469 4.83337 29 4.83337C15.6532 4.83337 4.83337 15.6532 4.83337 29C4.83337 42.3469 15.6532 53.1667 29 53.1667Z" stroke="#EF8C62" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M21.9675 21.7499C22.5357 20.1348 23.6571 18.7728 25.1333 17.9053C26.6094 17.0378 28.3449 16.7207 30.0324 17.0101C31.7199 17.2996 33.2505 18.1769 34.3531 19.4868C35.4558 20.7966 36.0592 22.4544 36.0567 24.1666C36.0567 28.9999 28.8067 31.4166 28.8067 31.4166" stroke="#EF8C62" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M29 41.0833H29.0233" stroke="#EF8C62" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+
+);
+}
+function BellRing() { //Wrapper - notification
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M18 8C18 6.4087 17.3679 4.88258 16.2426 3.75736C15.1174 2.63214 13.5913 2 12 2C10.4087 2 8.88258 2.63214 7.75736 3.75736C6.63214 4.88258 6 6.4087 6 8C6 15 3 17 3 17H21C21 17 18 15 18 8Z" stroke="#2147A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M13.73 21C13.5542 21.3031 13.3019 21.5547 12.9982 21.7295C12.6946 21.9044 12.3504 21.9965 12 21.9965C11.6496 21.9965 11.3054 21.9044 11.0018 21.7295C10.6982 21.5547 10.4458 21.3031 10.27 21" stroke="#2147A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M2 8C2 5.8 2.7 3.7 4 2" stroke="#2147A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M22 8C22 5.8363 21.2982 3.73096 20 2" stroke="#2147A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+
+);
+}
+function Calculator() {
+    return (
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M11.25 1.25H3.75C3.05964 1.25 2.5 1.80964 2.5 2.5V12.5C2.5 13.1904 3.05964 13.75 3.75 13.75H11.25C11.9404 13.75 12.5 13.1904 12.5 12.5V2.5C12.5 1.80964 11.9404 1.25 11.25 1.25Z" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M5 3.75H10" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M10 8.75V11.25" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M10 6.25H10.0063" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M7.5 6.25H7.50625" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M5 6.25H5.00625" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M7.5 8.75H7.50625" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M5 8.75H5.00625" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M7.5 11.25H7.50625" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M5 11.25H5.00625" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+);
+}
+function Catalogs() { //navbar - catalogs
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M11 3V11L14 8L17 11V3" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+);
+}
+function Close() { //Wrapper - notification
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M18 6L6 18" stroke="#0D152C" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M6 6L18 18" stroke="#0D152C" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+);
+}
+function Conclusions() { //conclusion navbar icon
+    return (
+        <svg
+            width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    >
+    <path
+        d="M12.0835 1.66675H5.00016C4.55814 1.66675 4.13421 1.84234 3.82165 2.1549C3.50909 2.46746 3.3335 2.89139 3.3335 3.33341V16.6667C3.3335 17.1088 3.50909 17.5327 3.82165 17.8453C4.13421 18.1578 4.55814 18.3334 5.00016 18.3334H15.0002C15.4422 18.3334 15.8661 18.1578 16.1787 17.8453C16.4912 17.5327 16.6668 17.1088 16.6668 16.6667V6.25008L12.0835 1.66675Z"
+    stroke="white"
+    strokeWidth="1.2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    />
+    <path
+        d="M11.6665 1.66675V6.66675H16.6665"
+    stroke="white"
+    strokeWidth="1.2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    />
+    <path
+        d="M7.5 12.4999L9.16667 14.1666L12.5 10.8333"
+    stroke="white"
+    strokeWidth="1.2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+        />
+        </svg>
+);
+}
+function CoverLetters() {//cover-letters navbar icon
+    return (
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M12.5003 1.66663H7.50033C7.04009 1.66663 6.66699 2.03972 6.66699 2.49996V4.16663C6.66699 4.62686 7.04009 4.99996 7.50033 4.99996H12.5003C12.9606 4.99996 13.3337 4.62686 13.3337 4.16663V2.49996C13.3337 2.03972 12.9606 1.66663 12.5003 1.66663Z"
+    stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+    <path
+        d="M6.66634 3.33337H4.99967C4.55765 3.33337 4.13372 3.50897 3.82116 3.82153C3.5086 4.13409 3.33301 4.55801 3.33301 5.00004V16.6667C3.33301 17.1087 3.5086 17.5327 3.82116 17.8452C4.13372 18.1578 4.55765 18.3334 4.99967 18.3334H14.9997C15.4417 18.3334 15.8656 18.1578 16.1782 17.8452C16.4907 17.5327 16.6663 17.1087 16.6663 16.6667V16.25"
+    stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+    <path
+        d="M13.333 3.33337H14.9997C15.2919 3.33367 15.579 3.41083 15.832 3.55709C16.0851 3.70336 16.2952 3.91359 16.4413 4.16671"
+    stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+    <path
+        d="M15.3497 8.00832C15.5122 7.84581 15.7051 7.71689 15.9174 7.62894C16.1298 7.54099 16.3574 7.49573 16.5872 7.49573C16.817 7.49573 17.0446 7.54099 17.2569 7.62894C17.4692 7.71689 17.6622 7.84581 17.8247 8.00832C17.9872 8.17083 18.1161 8.36376 18.204 8.57609C18.292 8.78842 18.3373 9.01599 18.3373 9.24582C18.3373 9.47564 18.292 9.70322 18.204 9.91555C18.1161 10.1279 17.9872 10.3208 17.8247 10.4833L14.1247 14.1666L10.833 15L11.658 11.7083L15.3497 8.00832Z"
+    stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"
+    />
+    <path
+        d="M6.66699 15H7.50033" stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+);
+}
+
+function DatePickerCalendar() {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z" stroke="#4ABD9B" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M16 2V6" stroke="#4ABD9B" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M8 2V6" stroke="#4ABD9B" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M3 10H21" stroke="#4ABD9B" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M8 14H8.01" stroke="#4ABD9B" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M12 14H12.01" stroke="#4ABD9B" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M16 14H16.01" stroke="#4ABD9B" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M8 18H8.01" stroke="#4ABD9B" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M12 18H12.01" stroke="#4ABD9B" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M16 18H16.01" stroke="#4ABD9B" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+);
+}
+
